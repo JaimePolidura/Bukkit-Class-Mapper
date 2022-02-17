0205b6ab12e113cf82cbfef8dc862660d82b98dd
JaimeTruman <jaime.polidura@gmail.com>
2022-02-17 22:30:54 +0100

    aded support for optinal args & default values

--- src/main/java/es/jaimetruman/commands/newversion/CommandArgsObjectBuilder.java
+++ src/main/java/es/jaimetruman/commands/newversion/CommandArgsObjectBuilder.java
@@ -1,39 +1,79 @@
 package es.jaimetruman.commands.newversion;
 
 import es.jaimetruman.commands.Command;
+import org.apache.commons.lang.StringUtils;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandArgsObjectBuilder {
     public <T> T build(Command commandInfo, String[] actualArgs, Class<T> classObjectArgs) throws Exception {
         try{
             return tryToBuild(commandInfo, actualArgs, classObjectArgs);
         }catch (Exception e) {
-            throw new Exception("Incorrecto use: " + commandInfo.usage());
+            e.printStackTrace();
+
+            throw new Exception("Incorrect use: " + commandInfo.usage());
         }
     }
 
     private <T> T tryToBuild(Command commandInfo, String[] actualArgs, Class<T> classObjectArgs) throws Exception {
         String[] requiredArgs = commandInfo.args();
         T newInstance = classObjectArgs.newInstance();
+        StringBuilder stringBuilderText = new StringBuilder();
+        int requiredArgIndex = 0;
+        List<String> argumensAdded = new ArrayList<>();
 
-        for (int i = 0; i < requiredArgs.length; i++) {
-            String argName = requiredArgs[i];
+        for (int actualArgIndex = 0; actualArgIndex < actualArgs.length; actualArgIndex++) {
+            String requiredArgUnnormalized = requiredArgs[requiredArgIndex];
+            String requiredArgNameNormalized = normalizeArgName(requiredArgUnnormalized);
+            String argValue = actualArgs[actualArgIndex];
 
-            if(isOptional(argName) && i >= actualArgs.length) break;
+            AnnalizyngArgState currentState = getCurrentState(actualArgIndex, requiredArgUnnormalized, actualArgs);
 
-            String actualArg = actualArgs[i];
-
-            Field field = newInstance.getClass().getDeclaredField(argName);
-            field.setAccessible(true);
-            field.set(newInstance, parseStringToFieldType(field, actualArg));
+            switch (currentState){
+                case NORMAL:
+                    setField(newInstance, requiredArgNameNormalized, argValue);
+                    argumensAdded.add(requiredArgUnnormalized);
+                    requiredArgIndex++;
+                    break;
+                case TEXT:
+                    stringBuilderText.append(argValue).append(" ");
+                    break;
+                case TEXT_END:
+                    stringBuilderText.append(argValue);
+                    setField(newInstance, requiredArgNameNormalized, stringBuilderText.toString());
+                    argumensAdded.add(requiredArgUnnormalized);
+                    break;
+            }
         }
+
+        checkIfArgumentMissingOrThrowException(argumensAdded, commandInfo.args(), newInstance);
 
         return newInstance;
     }
 
-    private static boolean isOptional(String arg){
-        return arg.contains("[");
+    private String normalizeArgName(String arg){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < arg.length(); i++) {
+            if(arg.charAt(i) != '.'){
+                stringBuilder.append(arg.charAt(i));
+            }
+        }
+
+        String res = stringBuilder.toString();
+
+        return res.contains("[") ?
+                StringUtils.substringBetween(stringBuilder.toString(), "[", "]") :
+                res ;
+    }
+
+    private <T> void setField(T newInstance, String argName, String actualArg) throws Exception {
+        Field field = newInstance.getClass().getDeclaredField(argName);
+        field.setAccessible(true);
+        field.set(newInstance, parseStringToFieldType(field, actualArg));
     }
 
     private static Object parseStringToFieldType(Field field, String arg){
@@ -54,5 +94,49 @@ public class CommandArgsObjectBuilder {
         }
 
         return arg;
+    }
+
+    private AnnalizyngArgState getCurrentState(int indexOfActualArgs, String requiredArg, String[] actualArgs){
+        if(isLongText(requiredArg) && indexOfActualArgs + 1 >= actualArgs.length){
+            return AnnalizyngArgState.TEXT_END;
+        }else if(isLongText(requiredArg)){
+            return AnnalizyngArgState.TEXT;
+        }else{
+            return AnnalizyngArgState.NORMAL;
+        }
+    }
+
+    private boolean isLongText(String reqArg){
+        return reqArg.endsWith("...");
+    }
+
+    private void checkIfArgumentMissingOrThrowException(List<String> argumentsAdded, String[] unnormalizedRequiredArguments, Object instance) throws Exception {
+        for (String unnormalizedRequiredArgument : unnormalizedRequiredArguments) {
+            if(!argumentsAdded.contains(unnormalizedRequiredArgument) && isRequired(unnormalizedRequiredArgument)){
+                throw new IllegalArgumentException("skjaslkjaslk");
+            }else if(!argumentsAdded.contains(unnormalizedRequiredArgument) && hasDefaultValue(unnormalizedRequiredArgument)){
+                setField(instance, normalizeArgName(unnormalizedRequiredArgument), getDefaultValue(unnormalizedRequiredArgument));
+            }
+        }
+    }
+
+    private String getDefaultValue(String arg){
+        return StringUtils.substringBetween(arg, "¡", "!");
+    }
+
+    private boolean hasDefaultValue(String arg){
+        return arg.contains("!");
+    }
+
+    private boolean isOptional(String arg){
+        return arg.contains("[");
+    }
+
+    private boolean isRequired(String arg){
+        return !arg.contains("[");
+    }
+
+    private enum AnnalizyngArgState {
+        NORMAL, TEXT, TEXT_END;
     }
 }
